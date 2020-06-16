39cce0eb6717ebf432664263cb5e50888df8a4f3
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-06-16 14:50:49 -0400

    seed solution

---
+++ OBeautifulCode.DateTime.Recipes.Test/DateTimeExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DateTimeExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.DateTime.Recipes.Test
+{
+    using System;
+
+    using OBeautifulCode.Assertion.Recipes;
+
+    using Xunit;
+
+    /// <summary>
+    /// TODO: Starting point for new project.
+    /// </summary>
+    public static class DateTimeExtensionsTest
+    {
+        [Fact]
+        public static void Method___Should_do_something___When_called()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OBeautifulCode.DateTime
+
+[![Build status](https://ci.appveyor.com/api/projects/status/qj8q6v0fjn81n430?svg=true)](https://ci.appveyor.com/project/SurajGupta/obeautifulcode-datetime)
+
+[![Nuget status](https://img.shields.io/nuget/v/OBeautifulCode.DateTime.Recipes.DateTimeExtensions.svg)](https://www.nuget.org/packages/OBeautifulCode.DateTime.Recipes.DateTimeExtensions)  OBeautifulCode.DateTime.Recipes.DateTimeExtensions
